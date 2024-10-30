000e745056a9a57d48f39c0a6d0f0d53afdbbe22
Jesus Suarez <jedasuar@espol.edu.ec>
2024-10-30 13:58:36 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+const databaseURL = 'https://landing-10f91-default-rtdb.firebaseio.com/coleccion.json';
+
+let sendData = () => {
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+
+    // new Date().toLocaleString( locales, options )
+    data['saved'] = new Date().toLocaleString('es-CO', { timeZone: 'America/Guayaquil' })
+
+    fetch(databaseURL, {
+        method: 'POST', // Método de la solicitud
+        headers: {
+            'Content-Type': 'application/json' // Especifica que los datos están en formato JSON
+        },
+        body: JSON.stringify(data) // Convierte los datos a JSON
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Error en la solicitud: ${response.statusText}`);
+        }
+        return response.json(); // Procesa la respuesta como JSON
+    })
+    .then(result => {
+        alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
+        form.reset()
+    })
+    .catch(error => {
+        alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+    });
+}
+
+
 let ready = () => {
     console.log('DOM está listo')
     //debugger;
@@ -26,7 +58,9 @@ let loaded = () => {
                     easing: "linear",
                 }
             );
+            return;
         }
+        sendData();
         
     });
     console.log('Iframes e Images cargadas')
@@ -35,6 +69,3 @@ let loaded = () => {
 
 window.addEventListener("DOMContentLoaded", ready);
 window.addEventListener("load", loaded)
-
-
-
